1bb0e896fdf01c201601fc7f716c62fa6a9959b2
Igor <igorkost142@gmail.com>
2025-10-30 16:22:41 +0300

    add some styles to header

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,9 +1,24 @@
-
-
 const user = {
-    id: 1,
-    name: "John",
-    age: 25,
-}
+  id: 1,
+  name: "John",
+  age: 25,
+};
 
-console.log(user)
+console.log(user);
+
+console.log("start");
+
+setTimeout(() => {
+  console.log("timer-1");
+  setTimeout(() => {
+    console.log();
+    console.log("timer-2");
+  }, 2000);
+}, 3000);
+
+setTimeout(() => {
+  console.log("timer-3");
+}, 5000);
+
+console.log("end");
+
